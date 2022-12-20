f035c3ca176789d3a5ca88b9649843a4081d2d25
2596172453 <117826185+2596172453@users.noreply.github.com>
2022-12-21 00:49:56 +0800

    Add files via upload

---
+++ 判断质数.cpp
@@ -0,0 +1,34 @@
+```
+#include<stdio.h>
+int zhishu(int);
+int main()
+{
+	int a;
+	int p;
+	printf("请输入一个数字\n");
+	scanf_s("%d", &a);
+	p = zhishu(a);
+	if (p)
+		printf("此数字是质数\n");
+	else
+		printf("此数字不是质数\n");
+	return 0;
+}
+int zhishu(int a)
+{
+	int i;
+	int flag = 0;
+	for (i = 2;i < a / 2;i++)
+	{
+		if (a % i == 0)
+		{
+			flag = 0;
+			break;
+		}
+		else flag = 1;
+	}
+	if (flag == 0)
+		return 0;
+	else return 1;
+}
+```
