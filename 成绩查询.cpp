d52d77a35c5783091af45cd03a2be69931819f1d
xumyan <117826185+xumyan@users.noreply.github.com>
2022-12-21 23:39:34 +0800

    Add files via upload

---
+++ 成绩查询.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	char ch;
+	printf("请输入您的成绩评级\n");
+	scanf_s("%d", &ch);
+	switch (ch)
+	{
+		case'A':printf("您的成绩在90分以上！\n");break;
+		case'B':printf("您的成绩在80~90分之间！\n");break;
+		case'C':printf("您的成绩在70~80分之间！\n");break;
+		case'D':printf("您的成绩在60~70分之间！\n");break;
+		case'E':printf("您的成绩在60分以下！\n");break;
+		default:printf("请输入正确的成绩评级\n");break;
+	}
+	return 0;
+}
